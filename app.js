209084a5c29b123d1234754862dda6a42464e057
natsumisawa <natsumi.sawa@bizreach.co.jp>
2019-03-11 22:23:55 +0900

    ;)

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // scope: テンプレートに公開する プロパティやメソッドを定義するオブジェクト
 // controller: scopeをセットアップする関数
 var MyController = function ($scope) {
-  $scope.message = 'hello, world';
-  $scope.action = function () {
-    $scope.message = 'good, bye';
+  $scope.count = 0;
+  $scope.double = function ($event) {
+    console.log($event)
+    $scope.count = $scope.count * 2;
   };
 };
 var appModule = angular.module('app', []);
